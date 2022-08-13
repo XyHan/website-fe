0ba50edde42e0328a14ae0f3a4e6a6c4636cd600
Robin <xyhan_1@hotmail.com>
2022-08-13 20:20:39 +0200

    refactor(component): add dates to experiences and educations

--- src/experience/infrastructure/repository/experience.repository.ts
+++ src/experience/infrastructure/repository/experience.repository.ts
@@ -13,14 +13,14 @@ export class ExperienceRepository implements ExperienceRepositoryInterface {
               'Développeur web senior',
               'Leaders League',
               new Date(),
-              null,
+              new Date(),
               'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras congue risus nunc, ac accumsan dolor fringilla in. In urna erat, accumsan sed felis a, pretium dapibus ex. Donec non arcu sed massa fringilla bibendum at sed lacus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec id tristique eros. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque egestas purus diam, a facilisis dolor accumsan ac. Nulla eget metus eget metus pharetra dignissim vitae quis libero. Nunc imperdiet auctor eros, non pharetra eros hendrerit vel. Aenean euismod neque tortor. In commodo rhoncus leo non tincidunt. Pellentesque egestas, nunc nec fermentum iaculis, elit orci pharetra risus, a porta turpis orci bibendum erat. Curabitur vel lacus efficitur, iaculis tortor eu, convallis nisl.'
             ),
             new Job(
               'Développeur magento junior',
               'Squad - X2I',
               new Date(),
-              null,
+              new Date(),
               'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras congue risus nunc, ac accumsan dolor fringilla in. In urna erat, accumsan sed felis a, pretium dapibus ex. Donec non arcu sed massa fringilla bibendum at sed lacus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec id tristique eros. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque egestas purus diam, a facilisis dolor accumsan ac. Nulla eget metus eget metus pharetra dignissim vitae quis libero. Nunc imperdiet auctor eros, non pharetra eros hendrerit vel. Aenean euismod neque tortor. In commodo rhoncus leo non tincidunt. Pellentesque egestas, nunc nec fermentum iaculis, elit orci pharetra risus, a porta turpis orci bibendum erat. Curabitur vel lacus efficitur, iaculis tortor eu, convallis nisl.'
             ),
             new Job(
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
+import { LOCALE_ID } from '@angular/core';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+
 import { AppModule } from './app/app.module';
 import { environment } from './app/infrastructure/config/environments/environment';
+
+registerLocaleData(localeFr, 'fr');
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic()
+  .bootstrapModule(AppModule, { providers: [{ provide: LOCALE_ID, useValue: 'fr' }] })
   .catch(err => console.error(err));
